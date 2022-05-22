4016dce84c228ffc18e6b02e03046cbb82351839
kzarnowski <zarnowski@student.agh.edu.pl>
2022-05-22 14:19:21 +0200

    zamkniecie

--- P1/main.py
+++ P1/main.py
@@ -1,53 +1,38 @@
-from re import L
 import numpy as np
 import cv2 as cv
 from PIL import Image
 import matplotlib.pyplot as plt
-from sympy import Q
 
 
-def padding(originalImg, padSize):
-    padImg = np.zeros((N+2*padSize, M+2*padSize), dtype=np.uint8)
-    # Slicing
-    padImg[padSize:N+padSize, padSize:M+padSize] = originalImg
-    return padImg
+def padding(img, pad):
+    """
+    Rozszerza obraz wejściowy o marginesy w celu obliczeń na brzegach.
+    Przykład (pad = 1):
+
+                        1 1 2 3 3
+    1 2 3               1 1 2 3 3
+    4 5 6       =>      4 4 5 6 6
+    7 8 9               7 7 8 9 9
+                        7 7 8 9 9
+    """
+
+    pad_img = np.zeros((N+2*pad, M+2*pad), dtype=np.uint8)
+
+    # przekopiowanie wejsciowego obrazu
+    pad_img[pad:N+pad, pad:M+pad] = img
+
+    # # uzupełnienie marginesów po lewej i prawej stronie
+    pad_img[pad:pad+N, :pad] = img[:, 0].reshape(-1, 1)
+    pad_img[pad:pad+N, pad+M:] = img[:, M-1].reshape(-1, 1)
+
+    # uzupełnienie marginesów u góry i dołu
+    pad_img[:pad, :] = pad_img[pad, :]
+    pad_img[pad+N:, :] = pad_img[pad+N-1, :]
+
+    return pad_img
 
 
-def Erosion(padImg, kernel, size):
-    output = np.zeros((rows, columns), dtype=np.uint8)
-    for i in range(0, rows):
-        for j in range(0, columns):
-            # Slicing
-            portion = padImg[i:i+size, j:j+size]
-            # sum of Kernel and window
-            portion1 = portion.flatten()
-            portion2 = kernel.flatten()
-            p1 = (np.sum(portion1))
-            p2 = (np.sum(portion2))*255
-            # if Fit Condition Satisfies
-            if p1 == p2:
-                output[i, j] = 255
-            else:
-                output[i, j] = np.min(portion1)
-    return output
-
-
-def Dilation(padImg, size):
-    output = np.zeros((rows, columns), dtype=np.uint8)
-    for i in range(0, rows):
-        for j in range(0, columns):
-            # Slicing
-            portion = padImg[i:i+size, j:j+size]
-            portion1 = portion.flatten()
-            # if Hit Condition Satisfies
-            if 255 in portion1:
-                output[i, j] = 255
-            else:
-                output[i, j] = np.max(portion1)
-    return output
-
-
-def zad_3(img):
+def zad_3_2(img):
     R = int(input('Podaj promien maski: '))
 
     n = 2*R + 1
@@ -71,7 +56,112 @@ def zad_3(img):
     return output
 
 
+def erode(img, SE):
+    img = np.array(img, copy=True)
+    res = np.array(img, copy=True)
+
+    [N, M] = img.shape
+    n = SE.shape[0]
+
+    side = int((n - 1) / 2)
+
+    for i in range(side, N - side):
+        for j in range(side, M - side):
+            slc = img[i - side:i + side + 1, j - side:j + side + 1]
+            bool_idx = (SE == 1)
+            res[i, j] = min(slc[bool_idx])
+
+    return res
+
+
+def dilate(img, SE):
+    img = np.array(img, copy=True)
+    res = np.array(img, copy=True)
+
+    [N, M] = img.shape
+    n = SE.shape[0]
+
+    side = int((n - 1) / 2)
+
+    for i in range(side, N - side):
+        for j in range(side, M - side):
+            slc = img[i - side:i + side + 1, j - side:j + side + 1]
+            bool_idx = (SE == 1)
+            res[i, j] = max(slc[bool_idx])
+
+    return res
+
+
+def zad_3(img):
+    R = int(input('Podaj promien maski: '))
+
+    # Utworzenie elementu kołowego o zadanym promieniu
+    n = 2*R + 1
+    center = (R, R)
+    mask = np.zeros(shape=(n, n), dtype=np.uint8)
+
+    for x in range(n):
+        for y in range(n):
+            dist = ((center[0] - x)**2 + (center[1] - y)**2)**0.5
+            if dist <= R:
+                mask[x, y] = 1
+
+    print(mask)
+    # Poszerzenie obrazu
+    pad = R
+
+    # Dylacja
+    pad_img = padding(img, pad)
+    dilated = np.empty((N, M), dtype=np.uint8)
+
+    for y in range(pad, pad+N):
+        for x in range(pad, pad+M):
+            fig = pad_img[y-pad:y+pad+1, x-pad:x+pad+1]
+            dilated[y-pad, x-pad] = np.max(fig[mask == 1])
+
+    # Erozja
+    pad_img = padding(dilated, pad)
+    output = np.empty((N, M), dtype=np.uint8)
+    for y in range(pad, pad+N):
+        for x in range(pad, pad+M):
+            fig = pad_img[y-pad:y+pad+1, x-pad:x+pad+1]
+            output[y-pad, x-pad] = np.min(fig[mask == 1])
+
+    return output
+
+
 def zad_4(img):
+    def hitmiss(img, mask):
+        # macierz wyników, przechowuje informacje czy na danej pozycji
+        # wartości pikseli są poprawne na podstawie zadanej maski
+        res = np.zeros((3, 3), dtype=np.bool8)
+
+        output = np.zeros((N, M), dtype=np.uint8)
+
+        for y in range(1, N-1):
+            for x in range(1, M-1):
+                # wycinek
+                fig = img[y-1:y+2, x-1:x+2]
+
+                # wypełnienie macierzy wyników
+                for i in range(3):
+                    for j in range(3):
+                        if mask[i, j] == 1:
+                            res[i, j] = fig[i, j] == 1
+                        elif mask[i, j] == -1:
+                            res[i, j] = fig[i, j] == 0
+                        else:
+                            res[i, j] = 1
+
+                # piksel obrazu wyjściowego przyjmuje wartość True, gdy piksele
+                # obrazu zgadzają się z maską na wszystkich pozycjach
+                output[y, x] = np.all(res)
+
+                # resetujemy macierz wyników
+                res.fill(False)
+        return output
+
+    # Inicjalizacja maski
     mask_1 = np.array([[1, 1, 0], [1, -1, 0], [1, 0, -1]])
     mask_2 = np.array([[1, 1, 1], [1, -1, 0], [0, -1, 0]])
     output = np.zeros((N, M), dtype=np.bool8)
@@ -91,33 +181,12 @@ def zad_4(img):
     return output
 
 
-def hitmiss(img, mask):
-    [N, M] = img.shape
-    res = np.zeros((3, 3), dtype=np.bool8)
-
-    output = np.zeros((N, M), dtype=np.uint8)
-    for y in range(1, N-1):
-        for x in range(1, M-1):
-            fig = img[y-1:y+2, x-1:x+2]
-            for i in range(3):
-                for j in range(3):
-                    if mask[i, j] == 1:
-                        res[i, j] = fig[i, j] == 1
-                    elif mask[i, j] == -1:
-                        res[i, j] = fig[i, j] == 0
-                    else:
-                        res[i, j] = 1
-            output[y, x] = np.all(res)
-            res.fill(False)
-    return output
-
-
 def zad_1(img):
     output = np.empty(shape=img.shape)
 
     n = int(input("Podaj liczbe punktow: "))
     print("Podaj punkty, wspolrzedne oddzielone spacją np: 100 50")
-    p = []
+    p = []  # lista par współrzędnych
     for i in range(n):
         point = input(f"Punkt {i+1}: ").split()
         point[0] = int(point[0])
@@ -126,13 +195,18 @@ def zad_1(img):
     p.append([0, 0])
     p.append([255, 255])
 
+    # sortujemy rosnąco po intensywności wejściowej
     p = sorted(p, key=lambda point: point[0])
 
     for y in range(N):
         for x in range(M):
+            # sprawdzamy w którym przedziale jest intensywność wejściowa
             i = 0
             while(p[i+1][0] < img[y, x]):
                 i += 1
+
+            # dla tego przedziału obliczamy intensywność wyjściową
+            # z równania prostej przechodzącej przez dwa punkty
             a = (p[i][1] - p[i+1][1])/(p[i][0] - p[i+1][0])
             if mono:
                 output[y, x] = a*img[y, x] + (p[i][1] - a*p[i][0])
@@ -167,8 +241,6 @@ def zad_2(img):
         for x in range(M):
             output[y, x] = int(a*output[y, x] - a*u)
 
-    print(u, v, np.min(output), np.max(output))
-
     return output
 
 
@@ -201,10 +273,11 @@ if __name__ == '__main__':
         exit()
 
     output_img = Image.fromarray(np.uint8(output))
-    plt.subplot(121)
-    plt.imshow(img, cmap="gray")
-    plt.subplot(122)
-    plt.imshow(output, cmap="gray")
+    f, axarr = plt.subplots(1, 2)
+
+    axarr[0].imshow(img, cmap="gray")
+    axarr[1].imshow(output, cmap="gray")
+
     plt.show()
 
     output_img.save("P1/img/result.png")
--- test.py
+++ test.py
@@ -1,5 +1,26 @@
-if True:
-    x = 5
-else:
-    x = 4
-print(x)  
+import numpy as np
+
+N = M = 3
+
+
+def padding(img, pad):
+    pad_img = np.zeros((N+2*pad, M+2*pad), dtype=np.uint8)
+
+    # przekopiowanie wejsciowego obrazu
+    pad_img[pad:N+pad, pad:M+pad] = img
+
+    # # uzupełnienie marginesów po lewej i prawej stronie
+    pad_img[pad:pad+N, :pad] = img[:, 0].reshape(-1, 1)
+    pad_img[pad:pad+N, pad+M:] = img[:, M-1].reshape(-1, 1)
+
+    # uzupełnienie marginesów u góry i dołu
+    pad_img[:pad, :] = pad_img[pad, :]
+    pad_img[pad+N:, :] = pad_img[pad+N-1, :]
+
+    return pad_img
+
+
+a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+b = padding(a, 3)
+print(b)
